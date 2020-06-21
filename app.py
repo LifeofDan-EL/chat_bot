87364e8963d9423eee564a024f485e76edb667a0
lifeofdan_el <anomfuemedaniel@gmail.com>
2020-06-21 15:19:34 +0100

    found another way

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 #import files
-import os
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
@@ -19,5 +18,5 @@ def get_bot_response():
     userText = request.args.get('msg')    
     return str(bot.get_response(userText)) 
 if __name__ == "__main__":    
-    
-    app.run(port=os.getenv("PORT"))
+
+    app.run(port=80)
