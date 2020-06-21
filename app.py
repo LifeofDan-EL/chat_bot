923b35a525dc54ca26cf679897439411f41382c6
lifeofdan_el <anomfuemedaniel@gmail.com>
2020-06-21 01:22:50 +0100

    Flask chatbot working locally with UI.

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from chatbot import chatbot
+#import files
 from flask import Flask, render_template, request
-
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
 app = Flask(__name__)
-app.static_folder = 'static'
-
+bot = ChatBot("Dan-Bot")
+trainer = ListTrainer(bot)
+trainer.train(['what is your name?', 'My name is Dan-Bot'])
+trainer.train(['who are you?', 'I am a Bot.'])
+trainer = ChatterBotCorpusTrainer(bot)
+trainer.train("chatterbot.corpus.english")
 @app.route("/")
-def home():
-    return render_template("index.html")
-
+def index():    
+    return render_template("index.html") 
 @app.route("/get")
-def get_bot_response():
-    userText = request.args.get('msg')
-    return str(chatbot.get_response(userText))
-
-if __name__ == "__main__":
-    app.run() 
+def get_bot_response():    
+    userText = request.args.get('msg')    
+    return str(bot.get_response(userText)) 
+if __name__ == "__main__":    
+    app.run()
